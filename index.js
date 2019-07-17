ba115eef1bdb322b7154b2c98c72fce585097df5
Jordan Winslow <admin@jordanwinslow.me>
2019-07-17 23:45:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+function nestedTarget() {
+  return getFirstSelector('#nested').querySelector('.target')
+}
+function deepestChild() {
+  return document.querySelector("#grand-node").lastElementChild.textContent
+}
+function increaseRankBy(numberToIncreaseBy) {
+  
+  const rankedLists = document.querySelectorAll('.ranked-list')
+  
+  for (let topIndex = 0; topIndex < rankedLists.length; topIndex++) {
+    for (let lowerIndex = 0; lowerIndex < rankedLists[topIndex].querySelectorAll('li').length; lowerIndex++) {
+      
+      let updatedRank = parseInt(document.querySelectorAll('.ranked-list')[topIndex].querySelectorAll('li')[lowerIndex].textContent) + numberToIncreaseBy
+      
+      document.querySelectorAll('.ranked-list')[topIndex].querySelectorAll('li')[lowerIndex].textContent = updatedRank
+      
+    }
+  }
+}
